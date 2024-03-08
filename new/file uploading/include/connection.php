86468bc927b36a40c13e2bc5847e5296d6fde121
HASEEB SHAHID <148750512+95HASEEBSHAHID@users.noreply.github.com>
2024-03-08 12:50:04 +0500

    Create connection.php

---
+++ new/file uploading/include/connection.php
@@ -0,0 +1,15 @@
+<?php
+    $server = "localhost";
+    $username = "root";
+    $db_pass = "";
+    $dbname = "mywork";
+
+    $connection = mysqli_connect($server, $username, $db_pass, $dbname);
+
+    if (!$connection){
+        die("Failed to connect");
+    }else{
+        echo "database connected";
+    }
+
+?>
